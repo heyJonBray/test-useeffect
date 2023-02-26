d9d769a662e3fe5b967ca07c89be5cd4afa7c675
Jon Bray <jon@heyjonbray.com>
2023-02-26 15:44:10 -0500

    Demonstrate state mutation with counter;

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
-import { useState } from 'react';
+/* eslint-disable no-template-curly-in-string */
+import { useEffect, useState } from 'react';
 import './App.css';
 
 import { useFetch } from './useFetch';
 
+/**
+ * Calling a state setter to prevent constant re-rendering, using both a value
+ * and a function.
+ * Using a function to increment count prevents the useEffect from rendering
+ * on every instance of the counter increasing.
+ *
+ * NOTE: Using 'count' as dependency causes constant useEffect re-rendering.
+ *
+ * @returns Count incrementing by the second.
+ */
+const useStopwatch = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('useStopwatch useEffect');
+    const interval = setInterval(() => {
+      /**
+       * Calling state with a single value:
+       */
+      // console.log(`Count = ${count}`);
+      // setCount(count + 1);
+      /**
+       * Calling state with a function:
+       */
+      console.log(`Count = ${count}`);
+      setCount((prev) => prev + 1);
+    }, 1000);
+    return () => clearInterval(interval);
+  }, []);
+
+  return count;
+};
+
 function App() {
   const [url, setUrl] = useState(null);
+  const count = useStopwatch();
   const { data } = useFetch({ url, onSuccess: () => console.log('success') });
 
   console.log('App rendering');
@@ -12,6 +47,7 @@ function App() {
   return (
     <div className="App">
       <div>Hello</div>
+      <div>Count: {count}</div>
       <div>{JSON.stringify(data)}</div>
       <div>
         <button onClick={() => setUrl('bob.json')}>Bob</button>
